33824a82254e2533cb46434078f4995a7f3f8d36
SHREYAS SACHIN MAHAJAN <2024.shreyas.mahajan@ves.ac.in>
2026-09-07 14:40:27 +0530

    Update test_login.py
    updating test_login.py ..........

--- test_login.py
+++ test_login.py
@@ -1,1 +1,10 @@
+from login import login
 
+def test_valid_login():
+    assert login("admin@caresync.com", "admin123") == True
+
+def test_invalid_password():
+    assert login("admin@caresync.com", "wrong123") == False
+
+def test_invalid_user():
+    assert login("unknown@gmail.com", "admin123") == False
